1709293cfd69097bcb7dc58f2927c4eca053f75e
dv970406 <dv970406@gmail.com>
2022-12-30 11:03:48 +0900

    useContext 폴더 정리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import "./App.css";
+import ReactMemo_Parent from "./memoization/ReactMemo_Parent";
+import UseCallback from "./memoization/UseCallback";
 import UseMemo from "./memoization/UseMemo";
+import UseMemo_Object from "./memoization/UseMemo_Object";
+import UseContext from "./context/UseContext_Parent";
+import { ThemeContext } from "./context/context";
+import { useState } from "react";
+import UseContext_Parent from "./context/UseContext_Parent";
+import UseReducer from "./reducer/UseReducer";
 
 function App() {
-  return <UseMemo />;
+  // useContext 용도
+  //const [isDark, setIsDark] = useState(false);
+
+  return (
+    // <ThemeContext.Provider value={{ isDark, setIsDark }}>
+    <UseReducer />
+    // </ThemeContext.Provider>
+  );
 }
 
 export default App;
